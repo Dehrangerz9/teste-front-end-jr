4b2d18cde15e5409529bea4d1413f25f993cf5d1
Yume Fernandes <107142833+Dehrangerz9@users.noreply.github.com>
2024-07-01 16:10:37 +0000

    [FIX]CSS do footer não estava funcionando.

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,82 +1,84 @@
 import CategoriaProdutos from "./components/categoriaProdutos/categoriaProdutos";
 import ProdutosRelacionados from "./components/produtosRelacionados/produtosRelacionados";
 import CardCombackground from "./components/cardComBackground/cardComBackground";
-import defaultCardBackground from "../../assets/card-default-image.webp"
-import './dashboard.scss';
+import defaultCardBackground from "../../assets/card-default-image.webp";
+import "./dashboard.scss";
 import MarcasRelacionadas from "./components/marcasRelacionadas/marcasRelacionadas";
-import logoVtex from "../../assets/vtex-logo.webp"
-function Dashboard(){
-    return (
-        <div className="dashboard">
-            <CategoriaProdutos/>
-            <p className="linhas-laterais">Produtos Relacionados</p>
-            <ProdutosRelacionados
-                categorias={[
-                    { 
-                        nome: "CELULAR",
-                        linkAPI: "https://cors-anywhere.herokuapp.com/https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json" },
-                    {
-                        nome: "ACESSÓRIOS",
-                    },
-                    {
-                        nome: "TABLETS",
-                    },
-                    {
-                        nome: "NOTEBOOKS",
-                    },
-                    {
-                        nome: "TVS",
-                    },
-                    {
-                        nome: "VER TODOS",
-                    }
-                ]}
-            />
-            <div className="linha-cartoes">
-                <CardCombackground
-                    cardTitle = "Parceiros"
-                    cardSubtitle = "Lorem ipsum dolor sit amet, consectetur"
-                    buttonText="CONFIRA"
-                    cardImage={defaultCardBackground}
-                />
-                <CardCombackground
-                    cardTitle = "Parceiros"
-                    cardSubtitle = "Lorem ipsum dolor sit amet, consectetur"
-                    buttonText="CONFIRA"
-                    cardImage={defaultCardBackground}
-                />                
-            </div>
-            <p className="linhas-laterais">Produtos Relacionados</p>
-            <p className="subtitulo">Ver todos</p>
-            <div className="linha-cartoes">
-                <CardCombackground
-                    cardTitle = "Produtos"
-                    cardSubtitle = "Lorem ipsum dolor sit amet, consectetur"
-                    buttonText="CONFIRA"
-                    cardImage={defaultCardBackground}
-                />
-                <CardCombackground
-                    cardTitle = "Produtos"
-                    cardSubtitle = "Lorem ipsum dolor sit amet, consectetur"
-                    buttonText="CONFIRA"
-                    cardImage={defaultCardBackground}
-                />                
-            </div>
-            <p>Navegue Por Marcas</p>
-            <MarcasRelacionadas
-                listaDeMarcas={[
-                    { logoMarca: logoVtex },
-                    { logoMarca: logoVtex },
-                    { logoMarca: logoVtex },
-                    { logoMarca: logoVtex },
-                    { logoMarca: logoVtex },
-                    { logoMarca: logoVtex },
-                    { logoMarca: logoVtex },
-                    { logoMarca: logoVtex }
-                ]}
-            />
-        </div>
-    )
+import logoVtex from "../../assets/vtex-logo.webp";
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <CategoriaProdutos />
+      <p className="linhas-laterais">Produtos Relacionados</p>
+      <ProdutosRelacionados
+        categorias={[
+          {
+            nome: "CELULAR",
+            linkAPI:
+              "https://cors-anywhere.herokuapp.com/https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json",
+          },
+          {
+            nome: "ACESSÓRIOS",
+          },
+          {
+            nome: "TABLETS",
+          },
+          {
+            nome: "NOTEBOOKS",
+          },
+          {
+            nome: "TVS",
+          },
+          {
+            nome: "VER TODOS",
+          },
+        ]}
+      />
+      <div className="linha-cartoes">
+        <CardCombackground
+          cardTitle="Parceiros"
+          cardSubtitle="Lorem ipsum dolor sit amet, consectetur"
+          buttonText="CONFIRA"
+          cardImage={defaultCardBackground}
+        />
+        <CardCombackground
+          cardTitle="Parceiros"
+          cardSubtitle="Lorem ipsum dolor sit amet, consectetur"
+          buttonText="CONFIRA"
+          cardImage={defaultCardBackground}
+        />
+      </div>
+      <p className="linhas-laterais">Produtos Relacionados</p>
+      <p className="subtitulo">Ver todos</p>
+      <div className="linha-cartoes">
+        <CardCombackground
+          cardTitle="Produtos"
+          cardSubtitle="Lorem ipsum dolor sit amet, consectetur"
+          buttonText="CONFIRA"
+          cardImage={defaultCardBackground}
+        />
+        <CardCombackground
+          cardTitle="Produtos"
+          cardSubtitle="Lorem ipsum dolor sit amet, consectetur"
+          buttonText="CONFIRA"
+          cardImage={defaultCardBackground}
+        />
+      </div>
+      <p>Navegue Por Marcas</p>
+      <MarcasRelacionadas
+        listaDeMarcas={[
+          { logoMarca: logoVtex },
+          { logoMarca: logoVtex },
+          { logoMarca: logoVtex },
+          { logoMarca: logoVtex },
+          { logoMarca: logoVtex },
+          { logoMarca: logoVtex },
+          { logoMarca: logoVtex },
+          { logoMarca: logoVtex },
+        ]}
+      />
+    </div>
+  );
 }
 
 export default Dashboard;
